7be87d90003f72ce04def5d37977d427624b9dd5
chetan1yadav@gmail.com <chetan1yadav@gmail.com>
2020-07-01 14:34:43 +0530

    commit12

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,7 +11,7 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
-  private _productsUrl = '../assets/product.json';
+  private _productsUrl = '../assets/products.json';
   constructor(private _http:Http) {
  
   }
@@ -20,7 +20,7 @@ export class ProductService {
     return this._http.get(this._albumUrl).map((response)=><Album>response.json());
   }
 
-  getProducts():Observable<Product[]>{
+  getProducts():Observable<Product[]>{                
     return this._http.get(this._productsUrl).map(response=><Product[]>response.json());
   }
 }
